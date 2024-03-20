d838952b24defedd386995026fd0285522c19287
Nahla <nahlahesham300@gmail.com>
2024-03-20 12:41:00 +0200

    DAL For Department Module

---
+++ MVC_FirstProject.DAL/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC_FirstProject.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC_FirstProject.DAL.Data
+{
+    internal class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+         => optionsBuilder.UseSqlServer("Server = .; Database = MVCApplication; Trusted_Connectuin = True");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+        public DbSet<Department> Departments { get; set; }
+    }
+}
---
+++ MVC_FirstProject.DAL/Models/Department.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC_FirstProject.DAL.Models
+{
+    // Model
+    internal class Department
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public DateTime DateOfCreation { get; set; }
+
+    }
+}
