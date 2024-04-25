bc3d76e111384abdcd4e64a59edfae553b2453c0
Nahla <nahlahesham300@gmail.com>
2024-04-25 05:51:59 +0200

    Send Email

---
+++ MVC_FirstProject.PL/Services/EmailSender/EmailSender.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace MVC_FirstProject.PL.Services.EmailSender
+{
+	public class EmailSender : IEmailSender
+	{
+		private readonly IConfiguration _configuration;
+
+		public EmailSender(IConfiguration configuration) 
+		{
+			_configuration = configuration;
+		}
+		public async Task SendAsync(string form, string recipients, string subject, string body)
+		{
+			var senderEmail = _configuration["EmailSettings:SenderEmail"];
+			var senderPassword = _configuration["EmailSettings:senderPassword"];
+
+			var emailMessage = new MailMessage();
+			emailMessage.From = new MailAddress(form);
+			emailMessage.Subject = subject;
+			emailMessage.Body = $"<html><body>{body}</body></html>";
+			emailMessage.To.Add(recipients);
+			emailMessage.IsBodyHtml = true;
+
+			var smtpClient = new SmtpClient(_configuration["EmailSettings:SmtpClientServer"],int.Parse(_configuration["EmailSettings:SmtpClientPort"]))
+			{
+				Credentials = new NetworkCredential(senderEmail,senderPassword),
+				EnableSsl = true
+			};
+			await smtpClient.SendMailAsync(emailMessage);
+		}
+	}
+}
---
+++ MVC_FirstProject.PL/Services/EmailSender/IEmailSender.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace MVC_FirstProject.PL.Services.EmailSender
+{
+	public interface IEmailSender
+	{
+		Task SendAsync(string form, string recipients, string subject, string body);
+	}
+}
